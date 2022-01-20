903d35fb0cae1a39d491a5a28cf1aab4c9d069d2
gkamalbabu <tekkadorob@gmail.com>
2022-01-20 06:44:15 +0530

    got stuck with a lot of bugs, don't know how to fix it, cannot see the shopping items on the screem its totally empty screen

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,34 +1,14 @@
- import React from 'react';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
- import SHOP_DATA from './shop.data.js';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
- import CollectionPreview from '../../components/collection-preview/collection-preview.jsx';
-import { createPortal } from 'react-dom';
-
- class ShopPage extends React.Component {
-     constructor(props) {
-         super(props);
-
-         this.state = {
-             collections: SHOP_DATA
-         }
-        }
-
-        render() {
-           const {collections} = this.state;
-           return (
-           <div className='shop-page'>
-            
-            {collections.map(({id, ...othercollectionProps}) => (
-                  <CollectionPreview key={id} {...othercollectionProps}/>
-          ))}
-            
-            </div>);
-        }
-    }
-            
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} element={<CollectionsOverview/>} />
+    <Route path={`${match.path}/:collectionId`} element={<CollectionPage/>} />
+  </div>
+);
 
 export default ShopPage;
-        
-    
-
